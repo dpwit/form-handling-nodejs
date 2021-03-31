a6f18cf1c780597c11dc73ccc820fc8180ec4981
darrenwhatford <darrenwhatford@hastingsdirect.com>
2021-03-31 15:16:18 +0100

    Added email address field

--- app.js
+++ app.js
@@ -15,5 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.post('/submit', (req, res) => {
     console.log('Username:' + req.body.username)
     console.log('Password:' + req.body.password)
+    console.log('Email address:' + req.body.email)
     res.redirect('/')
 })
